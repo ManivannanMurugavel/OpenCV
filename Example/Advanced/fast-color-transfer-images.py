374c05aa82474b06794744a7317e2020d55b7ac1
Manivannan Murugavel <manivannanmca2012@gmail.com>
2018-04-17 12:52:17 +0530

    fast color transfer

---
+++ Example/Advanced/fast-color-transfer-images.py
@@ -0,0 +1,72 @@
+import cv2
+import numpy as np
+
+
+def color_transfer(source, target):
+	# convert the images from the RGB to L*ab* color space, being
+	# sure to utilizing the floating point data type (note: OpenCV
+	# expects floats to be 32-bit, so use that instead of 64-bit)
+	source = cv2.cvtColor(source, cv2.COLOR_BGR2LAB).astype("float32")
+	target = cv2.cvtColor(target, cv2.COLOR_BGR2LAB).astype("float32")
+	# compute color statistics for the source and target images
+	(lMeanSrc, lStdSrc, aMeanSrc, aStdSrc, bMeanSrc, bStdSrc) = image_stats(source)
+	(lMeanTar, lStdTar, aMeanTar, aStdTar, bMeanTar, bStdTar) = image_stats(target)
+ 
+	# subtract the means from the target image
+	(l, a, b) = cv2.split(target)
+	l -= lMeanTar
+	a -= aMeanTar
+	b -= bMeanTar
+ 
+	# scale by the standard deviations
+	l = (lStdTar / lStdSrc) * l
+	a = (aStdTar / aStdSrc) * a
+	b = (bStdTar / bStdSrc) * b
+ 
+	# add in the source mean
+	l += lMeanSrc
+	a += aMeanSrc
+	b += bMeanSrc
+ 
+	# clip the pixel intensities to [0, 255] if they fall outside
+	# this range
+	l = np.clip(l, 0, 255)
+	a = np.clip(a, 0, 255)
+	b = np.clip(b, 0, 255)
+ 
+	# merge the channels together and convert back to the RGB color
+	# space, being sure to utilize the 8-bit unsigned integer data
+	# type
+	transfer = cv2.merge([l, a, b])
+	transfer = cv2.cvtColor(transfer.astype("uint8"), cv2.COLOR_LAB2BGR)
+	
+	# return the color transferred image
+	return transfer
+
+
+def image_stats(image):
+	# compute the mean and standard deviation of each channel
+	(l, a, b) = cv2.split(image)
+	(lMean, lStd) = (l.mean(), l.std())
+	(aMean, aStd) = (a.mean(), a.std())
+	(bMean, bStd) = (b.mean(), b.std())
+ 
+	# return the color statistics
+	return (lMean, lStd, aMean, aStd, bMean, bStd)
+
+
+srcImg = cv2.imread('ocean_sunset.jpg')
+trgImg = cv2.imread('ocean_day.jpg')
+srcImg = cv2.resize(srcImg,(400,500))
+trgImg = cv2.resize(trgImg,(400,500))
+
+transferImg = color_transfer(srcImg,trgImg)
+
+
+
+cv2.imshow('Source Image',srcImg)
+cv2.imshow('Target Image',trgImg)
+cv2.imshow('Transfer Image',transferImg)
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
